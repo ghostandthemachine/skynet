9c8ac06a43b6aaa08f4976ba1d7fc8d6352d33a1
ghostandthemachine <jonrose08@gmail.com>
2013-11-21 23:57:22 -0800

    init commit

--- src/skynet/core.clj
+++ src/skynet/core.clj
@@ -1,6 +1,20 @@
 (ns skynet.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+; (def scores (sorted-map \a 1  \b 3 ...))
+
+;  simple binary encoding  
+; (def score-encoder (int-coder :bits 4))
+
+; (encode score-coder 3)
+; binary encoding of 3
+; => <Vector4 [0 0 1 1]>
+; (decode score-coder *1)
+; => 3
+
+; (def letter-coder
+  ; (class-coder :values (keys scores)))
+
+(def net
+  (neural-network :inputs 26
+                  :outputs 4
+                  :hidden-sizes [6]))
